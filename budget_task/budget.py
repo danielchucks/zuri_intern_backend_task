d0790521402b7a2563ff7155139604f51b8d947a
danielchucks <iamdanielchukwuma@gmail.com>
2021-04-17 21:07:59 +0100

    Updated the withdrawal method

--- budget_task/budget.py
+++ budget_task/budget.py
@@ -1,25 +1,45 @@
 
 class budget :
 
+   # The constructor to the class
    def __init__(self, category, amount):
       self.category = category
       self.amount = amount
+      self.account = list()
 
    # Class Methods
-   def deposit(self):
-      return 'Enter amount to deposite'
+   def deposit(self, amount):
+      """ A deposit method that accepts an amount and category to deposit into """
+      balance = 0
+      balance += amount
+      self.account.append({'amount': amount})
+      return 'You deposited #%d into %s account your balance is #%d' % (amount, self.category, balance)
 
-   def withdraw():
-      return 'Enter amount to withdraw'
+   def withdraw(self, amount):
+      """ A withdraw method accepts an amount and category 
+      to withdraw from, and also checks to see if the amount 
+      to withdraw doesn't exceed the balance the category 
+      """
+      # amount = int(input('Enter amount to withdraw: '))
+      if(self.check_balance(amount)):
+         self.account.append({})
+         return 'Take your cash'
+      return 'Insufficient fund'
 
-   def check_balance():
-      return 'You current balance is: '
-
+   
+   def check_balance( self, amount ):
+      """ A check balance method  """
+      balance = 2000
+      for item in self.account:
+         balance +=item['amount']
+      
+      return balance
 
 category = budget('Clothing', 1000)
-category_2 = budget('Feed', 2000)
+category_2 = budget('Food', 2000)
 category_3 = budget('Entertament', 3000)
 
-print(category.deposit())
-print(category_2.deposit())
-print(category_3.deposit())
+# print(category.deposit(1000))
+print(category.withdraw(1000))
+# print(category_2.deposit(2000))
+# print(category_3.deposit(3000))
